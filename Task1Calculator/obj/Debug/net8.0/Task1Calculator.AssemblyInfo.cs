5e92d31becd6c43a4367211a6aaa85b959ca55a5
fjord-an <jordan.pacey@yahoo.com.au>
2024-06-28 17:28:43 +1000

    starting on password project. added an example in comments

--- Task1Calculator/obj/Debug/net8.0/Task1Calculator.AssemblyInfo.cs
+++ Task1Calculator/obj/Debug/net8.0/Task1Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99803d1c1a3fde7a3a1793b86e47b81ac12ed631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141f4954386abe9088c42b85fe3c2aa9ac8274bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task3Password/Program.cs
+++ Task3Password/Program.cs
@@ -4,9 +4,89 @@ namespace Password
 {
     class Program
     {
+        //### NOTE! the .Any method can be used to check if a string contains a digit, letter, or special character
         static void Main()
         {
-            if()
             Console.WriteLine("Enter a password: ");
             string password = Console.ReadLine();
-            Console
+
+            if (password.Length < 8)
+            {
+                Console.WriteLine("Password must be at least 8 characters long.");
+            }
+            else
+            {
+                bool hasDigit = false;
+                bool hasLetter = false;
+                bool hasSpecialChar = false;
+
+                foreach (char i in password)
+                {
+                    if (char.IsDigit(i))
+                    {
+                        hasDigit = true;
+                    }
+                    else if (char.IsLetter(i))
+                    {
+                        hasLetter = true;
+                    }
+                    else
+                    {
+                        hasSpecialChar = true;
+                    }
+                }
+
+                if (hasDigit && hasLetter && hasSpecialChar)
+                {
+                    Console.WriteLine("Password is valid.");
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "Password must contain at least one digit, one letter, and one special character.");
+                }
+            }
+        }
+        // Example method (maybe incorrect for the quesiton, does not store the password in a variable and read it)
+        static void Example()
+        {
+            do
+            {
+                bool hasDigit = false;
+                bool hasLetter = false;
+                bool hasSpecialChar = false;
+
+
+                string user_input;
+                Console.WriteLine(
+                    "Please enter password, password should include a mix of digits, number and one special character");
+                user_input = Convert.ToString(Console.ReadLine());
+                foreach (char i in user_input)
+                    if (char.IsDigit(i))
+                    {
+                        hasDigit = true;
+                    }
+                    else if (char.IsLetter(i))
+                    {
+                        hasLetter = true;
+                    }
+                    else
+                    {
+                        hasSpecialChar = true;
+                    }
+
+                if (hasDigit && hasLetter && hasSpecialChar)
+                {
+                    Console.WriteLine("Access granted. Welcome user!");
+                    Console.WriteLine("Goodbye");
+                    Console.ReadLine();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect password, please try again.");
+                }
+            } while (true);
+        }
+    }
+}
